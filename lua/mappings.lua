d307043b9f0851eeee603e99369ecc514a6efbf1
angel <angelsolaorbaiceta@gmail.com>
2025-11-25 23:50:26 +0100

    Some more changes

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,11 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Telescope
+map("n", "<leader>ls", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "LSP document symbols" })
+map("n", "<leader>lw", "<cmd>Telescope lsp_workspace_symbols<cr>", { desc = "LSP workspace symbols" })
+map("n", "<leader>mp", "<cmd>Telescope man_pages<cr>", { desc = "telescope man pages" })
+map("n", "<leader>fr", "<cmd>Telescope registers<cr>", { desc = "telescope registers" })
+map("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")

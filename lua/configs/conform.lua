bd06641671f60ae88a0a3cc6bd655849ffd076c7
Angel <angel.sola@wave.com>
2025-11-25 16:22:35 +0100

    Add formatters for conform.nvim

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,17 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    go = { "goimports", "gofmt" },
+    css = { "prettier" },
+    html = { "prettier" },
+    javascript = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options

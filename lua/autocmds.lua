8e9e1b9a5436825c16f318b44182b6cbe6da602e
angel <angelsolaorbaiceta@gmail.com>
2025-11-25 22:19:40 +0100

    Flash yanked text

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,13 @@
 require "nvchad.autocmds"
+
+-- Flash yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank {
+      higroup = "IncSearch", -- highlight group to use
+      timeout = 200, -- duration in milliseconds
+      on_visual = true, -- flash in visual mode too
+    }
+  end,
+})

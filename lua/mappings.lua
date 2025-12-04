182590e950b62c0441388bfdade04a94f84a882e
angel <angelsolaorbaiceta@gmail.com>
2025-12-04 11:04:19 +0100

    add keymappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,5 +13,6 @@ map("n", "<leader>lw", "<cmd>Telescope lsp_workspace_symbols<cr>", { desc = "LSP
 map("n", "<leader>mp", "<cmd>Telescope man_pages<cr>", { desc = "telescope man pages" })
 map("n", "<leader>fr", "<cmd>Telescope registers<cr>", { desc = "telescope registers" })
 map("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
+map("n", "<leader>fk", "<cmd>Telescope keymaps<CR>", { desc = "Find keymaps" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")

1fee303c8e66ba42b3032cb3c9d278e7d8ca7a20
angel <angelsolaorbaiceta@gmail.com>
2025-12-20 15:23:43 +0100

    Add Zig's LS

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "gopls" }
+local servers = { "html", "cssls", "gopls", "zls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
 -- vim.lsp.config('gopls', { })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,6 +25,7 @@ return {
         "go",
         "javascript",
         "c",
+        "zig",
       },
     },
   },
